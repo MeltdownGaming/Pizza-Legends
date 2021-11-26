433420919f3c8b855dad6e1bb32ca3896f55fe47
Anay Mittal <anayttal@gmail.com>
2021-11-26 18:23:30 -0500

    Added a shadow to character

--- Overworld.js
+++ Overworld.js
@@ -1,3 +1,5 @@
+//Hero Loading for spritesheet cropping; Comment = "//Character"
+
 class Overworld {
     constructor(config) {
         this.element = config.element;
@@ -13,9 +15,10 @@ class Overworld {
         }
         image.src = "/images/maps/DemoLower.png";
 
+        const x = 5;
+        const y = 6;
+
         //Character
-        const x = 0;
-        const y = 0;
         const hero = new Image();
         hero.onload = () => {
             this.ctx.drawImage(
@@ -24,12 +27,30 @@ class Overworld {
                 0,              //Start top crop
                 32,             //Width of crop
                 32,             //Height of crop
-                x,              //Position
-                y,              //Position
+                x * 16 - 8,     //Position times 16 (cellsize), minus because the character isn't prefectly in the top-right of our image
+                y * 16 - 18,    //Position times 16 (cellsize), minus because the character isn't prefectly in the top-right of our image
                 32,             //Size
                 32              //Size
             );
         }
         hero.src = "/images/characters/people/hero.png";
+
+        //Character Shadow
+        const shadow = new Image();
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow,
+                0,
+                0,
+                32,
+                32,
+                x * 16 - 8,
+                y * 16 - 18,
+                32,
+                32
+            )
+        }
+        
+        shadow.src = "/images/characters/shadow.png";
     }
 }
