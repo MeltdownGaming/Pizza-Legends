ffc50fca2072875d4fadf151157d1b6ec13c0507
Anay Mittal <anayttal@gmail.com>
2021-11-26 18:00:57 -0500

    Loaded hero with correct cropping

--- Overworld.js
+++ Overworld.js
@@ -6,10 +6,30 @@ class Overworld {
     }
 
     init() {
+        //Background
         const image = new Image();
         image.onload = () => {
             this.ctx.drawImage(image, 0, 0);
         }
-        image.src = "/images/maps/DemoLower.png"
+        image.src = "/images/maps/DemoLower.png";
+
+        //Character
+        const x = 0;
+        const y = 0;
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0,              //Start left crop
+                0,              //Start top crop
+                32,             //Width of crop
+                32,             //Height of crop
+                x,              //Position
+                y,              //Position
+                32,             //Size
+                32              //Size
+            );
+        }
+        hero.src = "/images/characters/people/hero.png";
     }
 }
